a444b1328b95dd339f1a3997f2920eb85902e6ae
gaofeifan <gaofeifan_java@163.com>
2017-11-08 15:15:57 +0800

    添加pojo

---
+++ src/main/java/com/pj/auth/pojo/AuthPostMenu.java
@@ -0,0 +1,13 @@
+package com.pj.auth.pojo;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2017/11/8.
+ */
+public @Data
+class AuthPostMenu {
+    private Integer postId;
+
+    private Integer menuId;
+}
