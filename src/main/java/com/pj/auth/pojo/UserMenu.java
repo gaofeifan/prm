30f67637b967a916adcca9f7c14e2f8a2b34336e
gao xiang <x.gao@qq.com>
2017-12-27 16:50:58 +0800

    权限分配到人

---
+++ src/main/java/com/pj/auth/pojo/UserMenu.java
@@ -0,0 +1,40 @@
+
+package com.pj.auth.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**  
+* @desc: prm用户与菜单关系表，用于权限细化到人的功能 
+* @author: x.gao  
+* @createTime: 2017年12月27日 上午10:50:51  
+* @history:  
+* @version: v1.0    
+*/
+@Data @Table(name="user_menu")
+public class UserMenu {
+  
+  public UserMenu(){};
+  public  UserMenu(String userId){
+    this.userId=userId;
+  };
+  
+  @ApiModelProperty(value = "id")
+  @GeneratedValue(generator = "JDBC")
+  @Id
+  private Integer id;
+  
+  @Column(name="user_id")
+  private String userId;
+  
+  @Column(name="auth_id")
+  private Integer authId;
+
+}
+
